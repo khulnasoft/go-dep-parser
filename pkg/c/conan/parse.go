764dbb94bccda85701953f248a3ed2e6d5cd9754
NxPKG <iconmamundentist@gmail.com>
2024-07-28 23:59:38 +0600

    Update parse.go
    Signed-off-by: NxPKG <iconmamundentist@gmail.com>

--- pkg/c/conan/parse.go
+++ pkg/c/conan/parse.go
@@ -9,9 +9,9 @@ import (
 	"golang.org/x/exp/slices"
 	"golang.org/x/xerrors"
 
-	dio "github.com/aquasecurity/go-dep-parser/pkg/io"
-	"github.com/aquasecurity/go-dep-parser/pkg/log"
-	"github.com/aquasecurity/go-dep-parser/pkg/types"
+	dio "github.com/khulnasoft/go-dep-parser/pkg/io"
+	"github.com/khulnasoft/go-dep-parser/pkg/log"
+	"github.com/khulnasoft/go-dep-parser/pkg/types"
 )
 
 type LockFile struct {
